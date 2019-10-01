42cb254beffc1eda943ef10129f54cb9f18736f9
GabrielDecroly <vip.gabi2016@gmail.com>
2019-10-01 19:55:46 +0200

    Timestampç

--- smr2a.php
+++ smr2a.php
@@ -2,7 +2,9 @@ Gabriel
 
 <?php
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,date('Y-m-d H:i:s '));
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
 fclose($f)
 ?>
 
