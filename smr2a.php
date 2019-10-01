519737604b99aad938213cb460bc87a0ceeff256
GabrielDecroly <vip.gabi2016@gmail.com>
2019-10-01 20:19:26 +0200

    Formulario

--- smr2a.php
+++ smr2a.php
@@ -8,4 +8,18 @@ fwrite($f,"\r\n");
 fclose($f)
 ?>
 
+<?php
+if(isset($_GET['correo'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['correo']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
+?>
+
 <a href="http://192.168.0.137/smr2A_Jose/smr2a.php">Jose</a>
+
+<form>
+	<input name="correo" type="email">
+	<button>Enviar</button>
+</form>
