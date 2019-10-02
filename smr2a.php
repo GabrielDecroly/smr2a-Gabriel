4515347b22cd1e5f26b83e782936beab310af734
GabrielDecroly <vip.gabi2016@gmailcom>
2019-10-02 15:44:20 +0200

    Catalogo

--- smr2a.php
+++ smr2a.php
@@ -1,11 +1,14 @@
-Gabriel
 
+
+
+<a href="http://192.168.0.108/smr2a-VCR/smr2a.php">Victor Cabo</a>
+Gabriel
+<a href="http://192.168.0.111/smr2A_Jose/smr2a.php">Jeso</a>
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
-fclose($f)
 ?>
 
 <?php
@@ -13,13 +16,20 @@ if(isset($_GET['correo'])){
 	$f=fopen('suscriptores.txt','a');
 	fwrite($f,$_GET['correo']);
 	fwrite($f,"\r\n");
-	fclose($f);
 }
 ?>
 
-<a href="http://192.168.0.137/smr2A_Jose/smr2a.php">Jose</a>
-
+<html>
+<body>
+<h1>FORMULARIO</h1>
 <form>
-	<input name="correo" type="email">
-	<button>Enviar</button>
+	<p>
+		Correo electronico: <input type="email" name="correo">
+	</p>
+	<button>Enviar la informacion</button>
 </form>
+
+</html>
+<a href="catalogo.php">Listado compañeros</a>
+	
+</html>	
